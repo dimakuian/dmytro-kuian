a9fbc1833c87fc6ce45ee91061a21cf49ce76e1e
Dmytro Kuian <dimakuian93@gmail.com>
2022-06-23 01:08:55 +0300

    Create pojo class for all pets, and create Pets config

--- src/main/java/com/epam/spring/homework1/MainClass.java
+++ src/main/java/com/epam/spring/homework1/MainClass.java
@@ -1,6 +1,8 @@
 package com.epam.spring.homework1;
 
 import com.epam.spring.homework1.config.BeansConfig;
+import com.epam.spring.homework1.pet.Cheetah;
+import com.epam.spring.homework1.pet.Pet;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
@@ -8,5 +10,10 @@ public class MainClass {
     public static void main(String[] args) {
         ApplicationContext context = new AnnotationConfigApplicationContext(BeansConfig.class);
 
+        Pet pet = context.getBean(Pet.class);
+        pet.printPets();
+
+        Cheetah cheetah = context.getBean(Cheetah.class);
+        Cheetah otherCheetah = (Cheetah) context.getBean("getOtherCheetah");
     }
 }
