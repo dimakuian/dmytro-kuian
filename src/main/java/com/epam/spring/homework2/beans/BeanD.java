c247f8a07b12dcd384e3cbd1dd27e1c1cdc415d7
Dmytro Kuian <dimakuian93@gmail.com>
2022-06-24 19:45:04 +0300

    Created POJO classes for all (6) beans

---
+++ src/main/java/com/epam/spring/homework2/beans/BeanD.java
@@ -0,0 +1,17 @@
+package com.epam.spring.homework2.beans;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class BeanD {
+    private String name;
+    private int value;
+
+    @Override
+    public String toString() {
+        return "BeanA{" +
+                "name='" + name + '\'' +
+                ", value=" + value +
+                '}';
+    }
+}
