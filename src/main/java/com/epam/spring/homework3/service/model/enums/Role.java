c7d0c4fc62042005da838dc5c144dddaea5af4ea
Dmytro Kuian <dimakuian93@gmail.com>
2022-07-04 20:27:43 +0300

    Create ErrorType enum

--- src/main/java/com/epam/spring/homework3/service/model/enums/Role.java
+++ src/main/java/com/epam/spring/homework3/service/model/enums/Role.java
@@ -1,4 +1,6 @@
-package com.epam.spring.homework3.service.model;
+package com.epam.spring.homework3.service.model.enums;
+
+import com.epam.spring.homework3.service.model.User;
 
 public enum Role {
     ADMIN, CLIENT;
