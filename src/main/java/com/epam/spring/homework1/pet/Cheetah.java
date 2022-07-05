a021c5144b43598cbeba195188c15e5e40b83631
Dima Kuian <85834287+dimakuian@users.noreply.github.com>
2022-07-05 23:55:46 +0300

    Delete src/main/java/com/epam/spring/homework1 directory

--- src/main/java/com/epam/spring/homework1/pet/Cheetah.java
+++
@@ -1,9 +0,0 @@
-package com.epam.spring.homework1.pet;
-
-import org.springframework.core.annotation.Order;
-import org.springframework.stereotype.Component;
-
-@Component
-@Order(3)
-public class Cheetah implements Animal {
-}
