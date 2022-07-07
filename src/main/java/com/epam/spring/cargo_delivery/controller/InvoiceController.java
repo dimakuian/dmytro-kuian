35507a553117a1265d4e6bf57d59e26d2138a2b4
Dmytro Kuian <dimakuian93@gmail.com>
2022-07-07 19:12:27 +0300

    Create and implement InvoiceApi usage

--- src/main/java/com/epam/spring/cargo_delivery/controller/InvoiceController.java
+++ src/main/java/com/epam/spring/cargo_delivery/controller/InvoiceController.java
@@ -2,68 +2,50 @@ package com.epam.spring.cargo_delivery.controller;
 
 import com.epam.spring.cargo_delivery.controller.assembler.InvoiceAssembler;
 import com.epam.spring.cargo_delivery.controller.dto.InvoiceDTO;
-import com.epam.spring.cargo_delivery.controller.dto.group.OnCreate;
-import com.epam.spring.cargo_delivery.controller.dto.group.OnUpdate;
 import com.epam.spring.cargo_delivery.controller.model.InvoiceModel;
 import com.epam.spring.cargo_delivery.service.InvoiceService;
+import com.epam.spring.cargo_delivery.service.api.InvoiceApi;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
-@RequestMapping(value = "/invoice")
 @RequiredArgsConstructor
-public class InvoiceController {
+public class InvoiceController implements InvoiceApi {
 
   private final InvoiceService invoiceService;
   private final InvoiceAssembler invoiceAssembler;
 
-  @ResponseStatus(HttpStatus.OK)
-  @GetMapping
+  @Override
   public List<InvoiceDTO> getAllInvoice() {
     return invoiceService.getInvoices();
   }
 
-  @ResponseStatus(HttpStatus.OK)
-  @GetMapping(value = "/{id}")
-  public InvoiceModel getInvoice(@PathVariable long id) {
+  @Override
+  public InvoiceModel getInvoice(long id) {
     InvoiceDTO outInvoiceDto = invoiceService.getInvoice(id);
     return invoiceAssembler.toModel(outInvoiceDto);
   }
 
-  @ResponseStatus(HttpStatus.CREATED)
-  @PostMapping
-  public InvoiceModel createInvoice(@RequestBody @Validated(OnCreate.class) InvoiceDTO invoiceDTO) {
+  @Override
+  public InvoiceModel createInvoice(InvoiceDTO invoiceDTO) {
     InvoiceDTO outInvoiceDto = invoiceService.createInvoice(invoiceDTO);
     return invoiceAssembler.toModel(outInvoiceDto);
   }
 
-  @ResponseStatus(HttpStatus.OK)
-  @PatchMapping(value = "/{id}")
-  public InvoiceModel updateInvoice(@PathVariable long id,
-      @RequestBody @Validated(OnUpdate.class) InvoiceDTO invoiceDTO) {
+  @Override
+  public InvoiceModel updateInvoice(long id, InvoiceDTO invoiceDTO) {
     log.info("Update invoice by id {}", id);
     log.trace("Request body invoiceDTO {}", invoiceDTO);
     InvoiceDTO outInvoiceDto = invoiceService.updateInvoice(id, invoiceDTO);
     return invoiceAssembler.toModel(outInvoiceDto);
   }
 
-  @ResponseStatus(HttpStatus.OK)
-  @DeleteMapping(value = "/{id}")
-  public ResponseEntity<Void> deleteInvoice(@PathVariable long id) {
+  @Override
+  public ResponseEntity<Void> deleteInvoice(long id) {
     log.info("Delete invoice by id {}", id);
     invoiceService.deleteInvoice(id);
     return ResponseEntity.noContent().build();
---
+++ src/main/java/com/epam/spring/cargo_delivery/service/api/InvoiceApi.java
@@ -0,0 +1,62 @@
+package com.epam.spring.cargo_delivery.service.api;
+
+import com.epam.spring.cargo_delivery.controller.dto.InvoiceDTO;
+import com.epam.spring.cargo_delivery.controller.dto.group.OnCreate;
+import com.epam.spring.cargo_delivery.controller.dto.group.OnUpdate;
+import com.epam.spring.cargo_delivery.controller.model.InvoiceModel;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
+import java.util.List;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@Api(tags = "Order management API")
+@RequestMapping("api/v1/invoice")
+public interface InvoiceApi {
+
+  @ApiOperation("Get all invoice")
+  @ResponseStatus(HttpStatus.OK)
+  @GetMapping
+  List<InvoiceDTO> getAllInvoice();
+
+  @ApiImplicitParams({
+      @ApiImplicitParam(name = "id", paramType = "path", required = true, value = "Invoice id")
+  })
+  @ApiOperation("Get invoice")
+  @ResponseStatus(HttpStatus.OK)
+  @GetMapping(value = "/{id}")
+  InvoiceModel getInvoice(@PathVariable long id);
+
+  @ApiOperation("Create invoice")
+  @ResponseStatus(HttpStatus.CREATED)
+  @PostMapping
+  InvoiceModel createInvoice(@RequestBody @Validated(OnCreate.class) InvoiceDTO invoiceDTO);
+
+  @ApiImplicitParams({
+      @ApiImplicitParam(name = "id", paramType = "path", required = true, value = "Invoice id")
+  })
+  @ApiOperation("Update invoice")
+  @ResponseStatus(HttpStatus.OK)
+  @PatchMapping(value = "/{id}")
+  InvoiceModel updateInvoice(@PathVariable long id,
+      @RequestBody @Validated(OnUpdate.class) InvoiceDTO invoiceDTO);
+
+  @ApiImplicitParams({
+      @ApiImplicitParam(name = "id", paramType = "path", required = true, value = "Invoice id")
+  })
+  @ApiOperation("Delete invoice")
+  @ResponseStatus(HttpStatus.OK)
+  @DeleteMapping(value = "/{id}")
+  ResponseEntity<Void> deleteInvoice(@PathVariable long id);
+}
