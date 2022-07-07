c2fc0a99ffbabbdac7012d463ebc811b0776930f
Dmytro Kuian <dimakuian93@gmail.com>
2022-07-07 19:02:32 +0300

    Create and implement OrderApi usage

--- src/main/java/com/epam/spring/cargo_delivery/controller/OrderController.java
+++ src/main/java/com/epam/spring/cargo_delivery/controller/OrderController.java
@@ -2,68 +2,50 @@ package com.epam.spring.cargo_delivery.controller;
 
 import com.epam.spring.cargo_delivery.controller.assembler.OrderAssembler;
 import com.epam.spring.cargo_delivery.controller.dto.OrderDTO;
-import com.epam.spring.cargo_delivery.controller.dto.group.OnCreate;
-import com.epam.spring.cargo_delivery.controller.dto.group.OnUpdate;
 import com.epam.spring.cargo_delivery.controller.model.OrderModel;
 import com.epam.spring.cargo_delivery.service.OrderService;
+import com.epam.spring.cargo_delivery.service.api.OrderApi;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
 @RequiredArgsConstructor
-@RequestMapping(value = "/order")
-public class OrderController {
+public class OrderController implements OrderApi {
 
   private final OrderService orderService;
   private final OrderAssembler orderAssembler;
 
-  @ResponseStatus(HttpStatus.OK)
-  @GetMapping
+  @Override
   public List<OrderDTO> getAllOrders() {
     return orderService.getOrders();
   }
 
-  @ResponseStatus(HttpStatus.OK)
-  @GetMapping(value = "/{id}")
-  public OrderModel getOrder(@PathVariable long id) {
+  @Override
+  public OrderModel getOrder(long id) {
     OrderDTO outOrderDto = orderService.getOrder(id);
     return orderAssembler.toModel(outOrderDto);
   }
 
-  @ResponseStatus(HttpStatus.CREATED)
-  @PostMapping
-  public OrderModel createOrder(@RequestBody @Validated(OnCreate.class) OrderDTO orderDTO) {
+  @Override
+  public OrderModel createOrder(OrderDTO orderDTO) {
     OrderDTO outOrderDto = orderService.createOrder(orderDTO);
     return orderAssembler.toModel(outOrderDto);
   }
 
-  @ResponseStatus(HttpStatus.OK)
-  @PatchMapping(value = "/{id}")
-  public OrderModel updateOrder(@PathVariable long id,
-      @RequestBody @Validated(OnUpdate.class) OrderDTO orderDTO) {
+  @Override
+  public OrderModel updateOrder(long id, OrderDTO orderDTO) {
     log.info("Update order by id {}", id);
     log.trace("Request body orderDTO {}", orderDTO);
     OrderDTO outOrderDto = orderService.updateOrder(id, orderDTO);
     return orderAssembler.toModel(outOrderDto);
   }
 
-  @ResponseStatus(HttpStatus.OK)
-  @DeleteMapping(value = "/{id}")
-  public ResponseEntity<Void> deleteOrder(@PathVariable long id) {
+  @Override
+  public ResponseEntity<Void> deleteOrder(long id) {
     log.info("Delete order by id {}", id);
     orderService.deleteOrder(id);
     return ResponseEntity.noContent().build();
---
+++ src/main/java/com/epam/spring/cargo_delivery/service/api/OrderApi.java
@@ -0,0 +1,63 @@
+package com.epam.spring.cargo_delivery.service.api;
+
+import com.epam.spring.cargo_delivery.controller.dto.OrderDTO;
+import com.epam.spring.cargo_delivery.controller.dto.group.OnCreate;
+import com.epam.spring.cargo_delivery.controller.dto.group.OnUpdate;
+import com.epam.spring.cargo_delivery.controller.model.OrderModel;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
+import java.util.List;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@Api(tags = "Order management API")
+@RequestMapping("api/v1/order")
+public interface OrderApi {
+
+  @ApiOperation("Get all order")
+  @ResponseStatus(HttpStatus.OK)
+  @GetMapping
+  List<OrderDTO> getAllOrders();
+
+  @ApiImplicitParams({
+      @ApiImplicitParam(name = "id", paramType = "path", required = true, value = "Order id")
+  })
+  @ApiOperation("Get order")
+  @ResponseStatus(HttpStatus.OK)
+  @GetMapping(value = "/{id}")
+  OrderModel getOrder(@PathVariable long id);
+
+  @ApiOperation("Create order")
+  @ResponseStatus(HttpStatus.CREATED)
+  @PostMapping
+  OrderModel createOrder(@RequestBody @Validated(OnCreate.class) OrderDTO orderDTO);
+
+  @ApiImplicitParams({
+      @ApiImplicitParam(name = "id", paramType = "path", required = true, value = "Order id")
+  })
+  @ApiOperation("Update order")
+  @ResponseStatus(HttpStatus.OK)
+  @PatchMapping(value = "/{id}")
+  OrderModel updateOrder(@PathVariable long id,
+      @RequestBody @Validated(OnUpdate.class) OrderDTO orderDTO);
+
+  @ApiImplicitParams({
+      @ApiImplicitParam(name = "id", paramType = "path", required = true, value = "Order id")
+  })
+  @ApiOperation("Delete order")
+  @ResponseStatus(HttpStatus.OK)
+  @DeleteMapping(value = "/{id}")
+  ResponseEntity<Void> deleteOrder(@PathVariable long id);
+
+}
